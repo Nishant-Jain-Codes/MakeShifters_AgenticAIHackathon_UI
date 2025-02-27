5660ac18c121997c91b5b74e4ed81aaf2b8a9a05
Nishant Jain <nishant.jain@salescode.ai>
2025-02-27 18:01:46 +0530

    added basic routing

--- mcd-kiosk/src/App.tsx
+++ mcd-kiosk/src/App.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import OrderTypeSelectionScreen from './screens/OrderTypeSelectionScreen';
+import MenuScreen from './screens/MenuScreen';
+import ItemViewScreen from './screens/ItemViewScreen';
+import OrderSummaryScreen from './screens/OrderSummaryScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import OrderCompletionScreen from './screens/OrderCompletionScreen';
 import "./App.css";
 import axios from "axios";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import useMenuStore from "./store/useMenuStore";
-import Home from "./pages/Home";
 import Loader from "./components/Loader";
 
 function App() {
@@ -52,9 +57,16 @@ function App() {
   };
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <Layout />,
-      children: [{ path: "/", element: <Home /> }],
+      children: [
+        { path: '/', element: <OrderTypeSelectionScreen /> },
+        { path: '/menu', element: <MenuScreen /> },
+        { path: '/items', element: <ItemViewScreen /> },
+        { path: '/summary', element: <OrderSummaryScreen /> },
+        { path: '/payment', element: <PaymentScreen /> },
+        { path: '/completion', element: <OrderCompletionScreen /> },
+      ],
     },
   ]);
 
