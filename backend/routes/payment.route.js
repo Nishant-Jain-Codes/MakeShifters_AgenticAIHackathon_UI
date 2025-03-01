957815aaefa8de9cacd0d8a4d1c1dde62a324f78
Rohit Mittal <rohit.mittal@salescode.ai>
2025-03-02 00:15:26 +0530

    removed the backend and bedrock

--- backend/routes/payment.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { createPayment } from "../controllers/payment.controller.js"
-
-const router=express.Router()
-
-router.post("/checkout",createPayment);
-
-
-export default router
